03b5499b4bd4a7a7aba11299a7b1c93bf0876664
jirhiker <jirhiker@gmail.com>
2014-03-27 16:41:33 -0600

    bumped version number

--- RemoteControlServer.cs
+++ RemoteControlServer.cs
@@ -18,7 +18,7 @@ under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 
-__version__=1.0
+__version__=1.0.1
 */
 using System.IO;
 using System.Text;
@@ -170,7 +170,7 @@ class RemoteControl
 		
 		string[] args = cmd.Trim().Split (' ');
 		string[] pargs;
-        string jargs;
+		string jargs;
 
 		double r;
 		switch (args[0]) {
