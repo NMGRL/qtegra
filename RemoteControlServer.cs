9cb11b9be3806e907b73f732c3be38db367c8dea
jirhiker <jirhiker@gmail.com>
2014-04-01 20:50:34 -0600

    added Slice

--- RemoteControlServer.cs
+++ RemoteControlServer.cs
@@ -1033,3 +1033,29 @@ class RemoteControl
 		return string.Empty;
 	}
 }
+
+//copied from http://www.dotnetperls.com/array-slice
+public static class Extensions
+{
+    /// <summary>
+    /// Get the array slice between the two indexes.
+    /// ... Inclusive for start index, exclusive for end index.
+    /// </summary>
+    public static T[] Slice<T>(this T[] source, int start, int end)
+    {
+	// Handles negative ends.
+	if (end < 0)
+	{
+	    end = source.Length + end;
+	}
+	int len = end - start;
+
+	// Return new array.
+	T[] res = new T[len];
+	for (int i = 0; i < len; i++)
+	{
+	    res[i] = source[i + start];
+	}
+	return res;
+    }
+}
